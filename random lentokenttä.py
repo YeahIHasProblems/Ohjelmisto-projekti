8244434304ce8ae2c07b36d439ce3607af4cd682
juustoo01 <juuso.myyra@gmail.com>
2026-02-25 17:14:47 +0200

    paikkojen arvonta yksinkertaistettu

--- random lentokenttä.py
+++ random lentokenttä.py
@@ -4,12 +4,11 @@ import mysql.connector
 
 
 def hae_satunnainen_lentokentta():
-    # This SQL picks a random country first, then a random airport in it
     sql = f"select airport.name as airport_name, country.name as country_name from airport join country on airport.iso_country = country.iso_country order by rand() limit 1;"
     print(sql)
     kursori = yhteys.cursor()
     kursori.execute(sql)
-    tulos = kursori.fetchone()  # Fetch only one result
+    tulos = kursori.fetchone()
 
     if tulos:
         print(f"Noutopaikka löydetty!")
@@ -18,7 +17,7 @@ def hae_satunnainen_lentokentta():
     else:
         print("Lentokenttää ei löytynyt.")
 
-# Create the connection (matching your screenshot)
+
 yhteys = mysql.connector.connect(
     host='127.0.0.1',
     port=3306,
@@ -27,5 +26,5 @@ yhteys = mysql.connector.connect(
     password='Maria1/26!',
     autocommit=True
 )
-# Test the new function
+
 hae_satunnainen_lentokentta()
