1fbed1eb9bdbc8cb9e049bb69216c840e2fc4509
Luka <luka.vesa@gmail.com>
2026-03-02 13:48:51 +0200

    top 1 EUNE yunara otp incoming

---
+++ kehari/laskuri.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+#toivottavasti toimii
+def laskuri():
+    score = 1
+    mult = 1
+    xmult = 1
+    sql = f"SELECT * FROM perks"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount >0 :
+        for rivi in tulos:
+            score += {rivi[0]}
+            mult += {rivi[1]}
+            xmult = xmult*{rivi[2]}
+        tulos = score*mult*xmult
+    return
+
+
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='Tekn11kan1hmelaps1',
+         autocommit=True
+         )
