f5046b6549569cb148287fcffc19ca95abf5f7d3
Samu Tolvanen <samu.s.tolvanen@gmail.com>
2026-03-10 19:23:28 +0200

    lisää testejä

--- Pääohjelma.py
+++ Pääohjelma.py
@@ -11,6 +11,10 @@ kauppasecurity = 0
 raha = 1000
 perkvalinta = ""
 perklist = []
+palkka = 0
+palkkastorage = 0
+double = 0
+roll = 0
 common = ["Common1", "Common2", "Common3"]
 rare = ["Rare1", "Rare2", "Rare3"]
 epic = ["Epic1", "Epic2", "Epic3"]
@@ -41,12 +45,10 @@ def hae_satunnainen_lentokentta():
     return kentta
 
 def lokaatio_update(lokaatio):
-    sql = f"UPDATE game SET location = (SELECT ident FROM airport WHERE name = '{lokaatio}') WHERE screen_name = 'Vesa';"
+    sql = f"UPDATE game SET location = (SELECT ident FROM airport WHERE name = '{lokaatio}') WHERE screen_name = 'Vesa' LIMIT 0;"
     kursori = yhteys.cursor()
     kursori.execute(sql)
-    tulos = kursori.fetchone()
-    return
-
+   
 nimi = "Vesa"
 def pelaajan_lokaatio(pelaaja):
     sql = f"SELECT name FROM airport, game where airport.ident = game.location and screen_name = '{pelaaja}'"
@@ -81,6 +83,22 @@ def perktulostus(perk):
                     f"-------------------------------------------\nNimi: {rivi[0]} Score: {rivi[1]} Mult: {rivi[2]} XMult: {rivi[3]}\n-------------------------------------------")
         return
 
+def laskuri(perklist):
+    score = 0
+    mult = 0
+    xmult = 1
+    for i in perklist:
+        sql = f"SELECT * FROM perks where name = '{i}'"
+        kursori = yhteys.cursor()
+        kursori.execute(sql)
+        tulos = kursori.fetchall()
+        if kursori.rowcount >0 :
+            for rivi in tulos:
+                score += rivi[1]
+                mult += rivi[2]
+                xmult = xmult*rivi[3]
+            tulos = score*mult*xmult
+    return tulos
 
 
 #valikko
@@ -92,28 +110,54 @@ def valikko():
 
 while True:
     valinta = valikko()
-
     if valinta == "1":
         lokaatio = hae_satunnainen_lentokentta()
         lokaatio_update(lokaatio)
-        pelaajan_lokaatio(nimi)
         kierros += 1
-        tavoite += 10
-        print(kierros)
         kauppasecurity = 0
+        palkka = random.randint(5,2000)
+        print("Tuleva palkka on "+ str(palkka))
+        double = int(input("Paina 1 jos haluat tuplata. Paina 0 jos et halua tuplata: "))
+        while double == 1:
+            roll = random.randint(1,2)
+            if roll == 1:
+                palkka = (palkka * 2)
+                print("Tuplaus onnistui, nykyinen palkka on "+ str(palkka))
+            else:
+                palkka = 0
+                print("Tuplaus huti, hävisit kaiken")
+                break
+            double = int(input("Paina 1 jos haluat tuplata uudestaan tai paina 0 jos haluat jatkaa: "))
+        raha += palkka
+        print("Nykyinen rahatilanne: "+ str(raha))
+        pelaajan_lokaatio(nimi)
+        if len(perklist)> 0:
+            tavoite += laskuri(perklist)
+        if tavoite >= 1000:
+            print("Voitit pelin")
+            print(f"Sinulla kesti {str(kierros)} kierrosta!")
+            break
+
+
+
+
+
     elif valinta == "2" and kauppasecurity == 0:
+        kauppalist.clear()
         while kaupparoll < 3:
             kauppalist.append(kauppa())
             perktulostus(kauppalist[-1])
             kaupparoll += 1
-        kauppasecurity += 1
+        kaupparoll = 0
+        kauppasecurity = 1
     elif valinta == "3":
-        perkvalinta = int(input("Minkä vaihtoehdon haluaisit ostaa"))
+        perkvalinta = int(input("Minkä vaihtoehdon haluaisit ostaa (1-3): "))
         if raha >= 1000:
             perklist.append(kauppalist[perkvalinta-1])
+            raha -= 1000
         else:
             print("Rahat ei riitä")
-        print(perklist)
+        print(f"Sinun perkit: {perklist}")
     elif valinta == "4":
         print("Lopetetaan ohjelma.")
         break
