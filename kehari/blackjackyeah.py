256d9e6582021cf6fa7678f627c4ff3c91a41477
Luka <luka.vesa@gmail.com>
2026-04-16 15:00:50 +0300

    toimi

---
+++ kehari/blackjackyeah.py
@@ -0,0 +1,59 @@
+import random
+import re
+deck = ["D2","D3","D4","D5","D6","D7","D8","D9","D10","D10","D10","D11",
+        "H2","H3","H4","H5","H6","H7","H8","H9","H10","H10","H10","H11",
+        "C2","C3","C4","C5","C6","C7","C8","C9","C10","C10","C10","C11",
+        "S2","S3","S4","S5","S6","S7","S8","S9","S10","S10","S10","S11"
+        ]
+totality = 0
+dealer = 0
+
+def pullcard(totality):
+    draw = random.randint(0,(len(deck)-1))
+    card = deck[draw]
+    country = re.findall("\\w", card)[0]
+    value = re.findall("\\d+", card)[0]
+    deck.remove(deck[draw])
+    if country == "C":
+        print(f"Your card is a " + value + " of Clubs")
+    elif country == "S":
+        print(f"Your card is a " + value + " of Spades")
+    elif country == "D":
+        print(f"Your card is a " + value + " of Diamonds")
+    elif country == "H":
+        print(f"Your card is a " + value + " of Hearts")
+    totality += int(value)
+    print("You're currently at " + str(totality))
+    return totality
+
+def dealer(deal):
+    if deal < 16:
+        draw = random.randint(0, (len(deck) - 1))
+        card = deck[draw]
+        country = re.findall("\\w", card)[0]
+        value = re.findall("\\d+", card)[0]
+        deck.remove(deck[draw])
+        if country == "C":
+            print(f"Dealer's card is a " + value + " of Clubs")
+        elif country == "S":
+            print(f"Dealer's card is a " + value + " of Spades")
+        elif country == "D":
+            print(f"Dealer's card is a " + value + " of Diamonds")
+        elif country == "H":
+            print(f"Dealer's card is a " + value + " of Hearts")
+    deal += value
+    print("The dealer is currently at " + dealer)
+    return deal
+
+while True:
+    decision = int(input("Wanna hit?"))
+    if decision == 1:
+        totality = pullcard(totality)
+        dealer = dealer(dealer)
+    if decision == 2:
+        break
+    if totality > 21:
+        print("Bust!")
+        break
+
+
