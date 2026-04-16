9eeb4598065b68f539445961e27a3bc0f23a4c02
juustoo01 <juuso.myyra@gmail.com>
2026-04-16 18:44:36 +0300

    pelin tallennus tietokantaan

---
+++ pelin tallennus.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+
+
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='root',
+    password='Maria1/26!',
+    autocommit=True
+)
+
+
+
+
+def tallenna_peli():
+    sql = "UPDATE game SET money = %s, tavoite = %s, location = (SELECT ident FROM airport WHERE name = %s LIMIT 1) WHERE screen_name = %s"
+    kursori = yhteys.cursor()
+    kursori.execute(sql, (raha, tavoite, lokaatio, nimi))
+    print(f"Peli tallennettu!")
