52b40c4291f02f8c6da8901d405633adb09b2bf1
juustoo01 <juuso.myyra@gmail.com>
2026-04-16 14:00:03 +0300

    kirjautumisvirhe testi

--- kirjautumis testaus.py
+++ kirjautumis testaus.py
@@ -1,3 +1,40 @@
+user_file= 'users.txt'
+
+def hash_salasana(salasana):
+    return hashlib.sha256(salasana.encode()).hexdigest()
+
+
+def users_olemassa(käyttäjänimi):
+    if not os.path.exists(user_file):
+        return False
+    with open(user_file, 'r') as f:
+        return any(line.startswith(f"{käyttäjänimi}:") for line in f)
+
+def register():
+    käyttäjänimi = input(" Anna käyttäjänimi: ")
+    if users_olemassa(käyttäjänimi):
+        print("Käyttäjä on jo olemassa")
+        return
+    Salasana = input(" Anna uusi salasana: ")
+    with open(user_file, 'a') as f:
+        f.write(f"{käyttäjänimi}:{hash_salasana(Salasana)}\n")
+        print("Rekisteröidytty onnistuneesti")
+
+def login():
+    if not os.path.exists(user_file):
+        print("Ei rekisteröityjä käyttäjiä")
+
+    käyttäjänimi=input("Anna käyttäjänimi: ")
+    salasana=input("Anna salasana: ")
+    hashed = hash_salasana(salasana)
+    with open(user_file, 'r') as f:
+        for line in f:
+            if line.strip() == f"{käyttäjänimi}:{hashed}":
+                print("Kirjauduttu onnistuneesti")
+                return True  # Tämä on tärkeä!
+        print("Kirjautuminen epäonnistui")
+
+
 def main():
     options = {'1':register, '2':login, '3': exit}
     while True:
