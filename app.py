1054aa5a86610cff190aec3680c1c9ae5d47ffcf
Samu Tolvanen <samu.s.tolvanen@gmail.com>
2026-05-02 15:49:49 +0300

    Todella alkeellinen testi flaskin toiminnasta

---
+++ app.py
@@ -0,0 +1,130 @@
+from flask import Flask, render_template, redirect, url_for, session, request
+from pelinlogiikka import hae_satunnainen_lentokentta, kauppa_roll, laskuri
+
+app = Flask(__name__)
+app.secret_key = "supersecret"  # vaihda johonkin omaan
+
+def reset_game():
+    session["kierros"] = 0
+    session["tavoite"] = 0
+    session["raha"] = 1000
+    session["perklist"] = []
+    session["kauppalist"] = []
+    session["kauppasecurity"] = 0
+    session["palkka"] = 0
+    session["double_active"] = False
+    session["double_message"] = ""
+    session["lokaatio"] = ""
+
+
+def init_game():
+    session.setdefault("kierros", 0)
+    session.setdefault("tavoite", 0)
+    session.setdefault("raha", 1000)
+    session.setdefault("perklist", [])
+    session.setdefault("kauppalist", [])
+    session.setdefault("kauppasecurity", 0)
+    session.setdefault("palkka", 0)
+    session.setdefault("double_active", False)
+    session.setdefault("lokaatio", "")
+
+@app.route("/")
+def index():
+    init_game()
+    return render_template("index.html")
+
+@app.route("/lento", methods=["GET", "POST"])
+def lento():
+    init_game()
+
+    if request.method == "POST":
+        action = request.form.get("action")
+
+        # Aloita lento
+        if action == "start":
+            session["lokaatio"] = hae_satunnainen_lentokentta()
+            session["palkka"] = random.randint(5, 2000)
+            session["double_active"] = True
+            session["double_message"] = ""
+            session["kierros"] += 1
+            session["kauppalist"] = []
+            session["kauppasecurity"] = 0
+            return redirect(url_for("lento"))
+
+        # Tuplaus
+        if action == "double":
+            roll = random.randint(1, 2)
+            if roll == 1:
+                session["palkka"] *= 2
+                session["double_message"] = "Tuplaus onnistui!"
+            else:
+                session["palkka"] = 0
+                session["double_message"] = "Tuplaus epäonnistui!"
+                session["double_active"] = False
+            return redirect(url_for("lento"))
+
+        # Lopeta tuplaus
+        if action == "stop_double":
+            session["raha"] += session["palkka"]
+
+            if session["perklist"]:
+                session["tavoite"] += laskuri(session["perklist"])
+
+            if session["tavoite"] >= 1000:
+                return redirect(url_for("voitto"))
+
+            #Nollaa lentotila
+            session["double_active"] = False
+            session["double_message"] = ""
+            session["lokaatio"] = ""
+            session["palkka"] = 0
+
+            return redirect(url_for("lento"))
+
+    return render_template("lento.html")
+
+
+
+
+@app.route("/kauppa")
+def kauppa_sivu():
+    init_game()
+    if session["kauppasecurity"] == 0:
+        session["kauppalist"] = [kauppa_roll() for _ in range(3)]
+        session["kauppasecurity"] = 1
+    return render_template("kauppa.html")
+
+@app.route("/osta_perk", methods=["POST"])
+def osta_perk():
+    init_game()
+    index = int(request.form.get("index"))
+    hinta = 1000
+
+    if session["raha"] >= hinta:
+        try:
+            perk_nimi = session["kauppalist"][index]
+            session["perklist"].append(perk_nimi)
+            session["raha"] -= hinta
+        except IndexError:
+            pass
+    return redirect(url_for("perkit"))
+
+@app.route("/perkit")
+def perkit():
+    init_game()
+    return render_template("perkit.html")
+
+@app.route("/voitto")
+def voitto():
+    kierrokset = session.get("kierros", 0)
+
+    # Nollaa peli kokonaan
+    reset_game()
+
+    return render_template("voitto.html", kierrokset=kierrokset)
+
+
+if __name__ == "__main__":
+    import random  # tarvitaan appissa
+    app.run(debug=True)
+
---
+++ pelinlogiikka.py
@@ -0,0 +1,90 @@
+import mysql.connector
+import random
+
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='root',
+    password='AdminST',
+    autocommit=True
+)
+common = ["Common1", "Common2", "Common3"]
+rare = ["Rare1", "Rare2", "Rare3"]
+epic = ["Epic1", "Epic2", "Epic3"]
+supergamble = ["Jackpot1", "Jackpot2", "Jackpot3"]
+
+
+def hae_satunnainen_lentokentta():
+    sql = """
+        SELECT airport.name 
+        FROM airport 
+        ORDER BY RAND() 
+        LIMIT 1;
+    """
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchone()
+    return tulos[0] if tulos else "Tuntematon kenttä"
+
+
+def lokaatio_update(lokaatio, pelaaja="Vesa"):
+    sql = """
+        UPDATE game 
+        SET location = (SELECT ident FROM airport WHERE name = %s) 
+        WHERE screen_name = %s;
+    """
+    kursori = yhteys.cursor()
+    kursori.execute(sql, (lokaatio, pelaaja))
+
+
+def hae_pelaajan_lokaatio(pelaaja="Vesa"):
+    sql = """
+        SELECT airport.name 
+        FROM airport 
+        JOIN game ON airport.ident = game.location 
+        WHERE screen_name = %s;
+    """
+    kursori = yhteys.cursor()
+    kursori.execute(sql, (pelaaja,))
+    tulos = kursori.fetchone()
+    return tulos[0] if tulos else None
+
+
+def kauppa_roll():
+    jackpot = random.randint(1, 100)
+
+    if jackpot < 60:
+        return random.choice(common)
+    elif jackpot < 85:
+        return random.choice(rare)
+    elif jackpot < 95:
+        return random.choice(epic)
+    else:
+        return random.choice(supergamble)
+
+
+def hae_perkin_tiedot(perk):
+    sql = "SELECT * FROM perks WHERE name = %s"
+    kursori = yhteys.cursor()
+    kursori.execute(sql, (perk,))
+    return kursori.fetchone()
+
+
+def laskuri(perklist):
+    score = 0
+    mult = 0
+    xmult = 1
+
+    for perk in perklist:
+        sql = "SELECT * FROM perks WHERE name = %s"
+        kursori = yhteys.cursor()
+        kursori.execute(sql, (perk,))
+        tulos = kursori.fetchone()
+
+        if tulos:
+            score += tulos[1]
+            mult += tulos[2]
+            xmult *= tulos[3]
+
+    return score * mult * xmult
